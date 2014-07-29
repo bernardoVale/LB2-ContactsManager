e1aecc427f879d3cde407f7b9a9448643a0875b8
bernardoVale <bernardosilveiravale@gmail.com>
2014-07-28 21:43:24 -0300

    Modelagem do Contato + Tests

--- lb2Contacts/contatos/models.py
+++ lb2Contacts/contatos/models.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import datetime
 from django.db import models
 
@@ -10,14 +11,38 @@ from lb2Contacts.settings import DBNAME
 connect(DBNAME)
 
 class Visita(EmbeddedDocument):
+    """
+    Define um momento em que um determinado usuário
+    interagiu ou visitou um contato da LB2.
+    """
+
     data = DateTimeField(required=True,default=datetime.datetime.now())
-    observacoes = StringField(required=True),
+    observacoes = StringField(required=True)
     tags = ListField(StringField(max_length=50))
     visitante = ReferenceField(User)
 
+class UserPrefs(EmbeddedDocument):
+    """
+    Preferências do usuário para um determinado contato
+    """
+
+    ALERTAS = ("SEMANAL","QUINZENAL","MENSAL","SEM ALERTA")
+
+    alerta = ListField(StringField(), choices=ALERTAS)
+    user = ReferenceField(User)
+
 class Contato(Document):
+    """
+    Documento pai que será gerido em todo o sistema.
+    Define um contato (cliente da LB2) que um ou mais usuários
+    poderam interagir.
+    """
+
     nome = StringField(max_length=60, required=True)
     email = EmailField(max_length=60)
     telefone = StringField(max_length=20)
+    celular = StringField(max_length=20)
+    endereco = StringField(max_length=80)
     skype = StringField(max_length=60, required=True)
     visitas = ListField(EmbeddedDocumentField(Visita))
+    user_prefs = ListField(EmbeddedDocumentField(UserPrefs))
--- lb2Contacts/contatos/tests.py
+++ lb2Contacts/contatos/tests.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from django.utils.unittest.case import TestCase
-from lb2Contacts.contatos.models import Contato
+from mongoengine.django.auth import User
+from lb2Contacts.contatos.models import Contato, Visita, UserPrefs
 
 
 class ContatoTestCase(TestCase):
@@ -11,10 +12,76 @@ class ContatoTestCase(TestCase):
             telefone = '4399651538',
             skype = 'joaoskype',
         ).save()
+        User.drop_collection()
+        self.u = User.create_user('wilmutt','vilmo','wilmutt@gmail.com')
 
     def test_total_objects(self):
+        """
+        Faz um teste de contagem de objetos
+        :return:
+        """
         total = Contato.objects.count()
         self.assertEqual(1,total)
 
     def test_contato_existente(self):
+        """
+        Verifica se o contato inserido no setup foi persistido
+        :return:
+        """
         self.assertEqual(self.contatoOK.nome,'Joao')
+
+    def test_createUser(self):
+        """
+        Testa a propriedade de criação de usuários
+        :return:
+        """
+        self.assertEqual(self.u.username,'wilmutt')
+
+    def test_novaVisita(self):
+        """
+        Adiciona uma visita ao Contato e verifica a persistencia.
+        :return:
+        """
+        visita = Visita (
+            observacoes = 'O nene caiu no estradon.',
+            tags = ['NENE','BIXO BURRU'],
+            visitante = self.u
+        )
+        self.contatoOK.update(add_to_set__visitas=visita)
+        c_before = self.contatoOK.save()
+        c = Contato.objects.get_or_create(nome='Joao')
+
+        self.assertNotEqual(c_before,c)
+        self.assertEqual(len(c[0].visitas[0].tags),2)
+        self.assertEqual(c[0].visitas[0].visitante.username,'wilmutt')
+        self.assertEqual(c[0].visitas[0].observacoes,'O nene caiu no estradon.')
+
+    def test_user_prefs(self):
+        """
+        Verifica se é possível adicionar as preferencias do usuário ao contato
+        :return:
+        """
+        user_prefs = UserPrefs(
+            user = self.u,
+            alerta = 'SEMANAL'
+        )
+        self.contatoOK.update(add_to_set__user_prefs=user_prefs)
+        self.contatoOK.save()
+        cc = Contato.objects.get(nome='Joao')
+        self.assertEqual(cc.user_prefs[0].user.username,'wilmutt')
+        self.assertEqual(cc.user_prefs[0].alerta,'SEMANAL')
+
+    def test_update_user_prefs(self):
+        """
+        Testa a atualização do alerta de um usuário de semanal para mensal
+        para um determinado contato.
+        :return:
+        """
+        user_prefs = UserPrefs(
+            user = self.u,
+            alerta = 'SEMANAL'
+        )
+        self.contatoOK.update(add_to_set__user_prefs=user_prefs)
+        self.contatoOK.save()
+        user_prefs.alerta = 'MENSAL'
+        Contato.objects(id=self.contatoOK.pk).update(set__user_prefs=user_prefs)
